43b2cf78cf8712448252d9b27d134aef82b0da4e
Mark de Neut Brossois <markdeneutbrossois@Marks-MacBook-Air.local>
2026-01-29 23:33:15 +0100

    Updated styling of dropdown

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Dropdown from "@/components/dropdown";
-import { useState } from "react";
+import { type SetStateAction, useState } from "react";
 
 const dayDropdown = [
   {value: 1, label: 'Next 1 hour'},
@@ -11,19 +11,54 @@ const dayDropdown = [
   {value: 6, label: 'Next 6 hour'}
 ]
 
+const durationDropdown = [
+  {value: 15, label: '15 minutes'},
+  {value: 30, label: '30 minutes'},
+  {value: 60, label: '1 hour'},
+]
+
 export default function Home() {
-  const [selected, setSelected] = useState("");
+  const [selectedTime, setSelectedTime] = useState('');
+  const [selectedDuration, setSelectedDuration] = useState('');
+  const animation = getAnimationState();
+
+  function getAnimationState() {
+    if(selectedTime === '') return 'time'
+    if(selectedDuration === '') return 'duration'
+    return ''
+  }
+
+  function updateEvent(variant: 'time' | 'duration', value: SetStateAction<string>) {
+    if(variant === 'time') {
+      setSelectedTime(value)
+    }
+
+    if(variant === 'duration') {
+      setSelectedDuration(value)
+    }
+  }
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center gap-8 py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Dropdown 
           name="day-selector" 
           placeholder="Select a time range" 
           label="In what time range do you want to walk your dog?" 
           options={dayDropdown}
-          selected={selected}
-          onSetSelected={value => setSelected(value)}
+          hasAnimation={animation === 'time'}
+          selected={selectedTime}
+          onSetSelected={value => updateEvent('time', value)}
+          ></Dropdown>
+
+        <Dropdown 
+          name="duration-selector" 
+          placeholder="Select a duration" 
+          label="What amount of time do you walk your dog?" 
+          options={durationDropdown}
+          hasAnimation={animation === 'duration'}
+          selected={selectedDuration}
+          onSetSelected={value => updateEvent('duration', value)}
           ></Dropdown>
       </main>
     </div>
--- components/dropdown.tsx
+++ components/dropdown.tsx
@@ -11,6 +11,7 @@ interface Dropdown {
   placeholder: string,
   options: Array<Options>,
   selected: string,
+  hasAnimation: boolean,
   onSetSelected: Dispatch<SetStateAction<string>>
 }
 
@@ -20,18 +21,31 @@ export default function Dropdown({
     placeholder,
     label,
     selected,
+    hasAnimation,
     onSetSelected
 }: Dropdown) {
   
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-2">
         <label htmlFor={name}>{label}</label>
-        <select defaultValue={selected} onChange={e => onSetSelected(e.target.value)} name={name} id={name} className="bg-indigo-700 text-fuchsia-100">
-          <option value="" disabled>{placeholder}</option>  
-          {options.map(option => 
-            <option value={option.value} key={option.value}>{option.label}</option>
-          )}
-        </select>
+        <div className={`w-fit rounded-md p-[1px] relative overflow-hidden z-1000 after:rounded-full
+          after:bg-conic-0 ${hasAnimation ? `after:animate-spin-slow after:from-purple-500 after:to-violet-400` : `after:from-purple-200 after:to-violet-100`}
+          after:absolute after:w-50 after:h-50 after:left-[50%] after:top-[50%] after:-translate-[50%] after:z-10`}>
+          <select 
+            defaultValue={selected} 
+            onChange={e => onSetSelected(e.target.value)} 
+            name={name} 
+            id={name} 
+            aria-label={label}
+            className="dropdown relative z-100 outline-none
+              p-2 bg-indigo-700 text-fuchsia-50 rounded-md
+              dark:bg-indigo-800 dark:text-indigo-100">
+            <option value="" disabled>{placeholder}</option>  
+            {options.map(option => 
+              <option value={option.value} key={option.value}>{option.label}</option>
+            )}
+          </select>
+        </div>
     </div>
   );
 }
