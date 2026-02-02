9d8da1c244c03bc631fd7cdbf2adb3db4c22b0ad
Mark de Neut Brossois <markdeneutbrossois@Marks-MacBook-Air.local>
2026-02-02 19:16:16 +0100

    Updated hover style + bigger border so its visible on hover

--- components/dropdown.tsx
+++ components/dropdown.tsx
@@ -1,4 +1,5 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
+import Chevron from "@/components/icons/chevron";
 
 interface Options {
   value: string | number,
@@ -24,13 +25,16 @@ export default function Dropdown({
     hasAnimation,
     onSetSelected
 }: Dropdown) {
+
+  const [isMouseOver, setMouseOver] = useState(false)
   
   return (
     <div className="flex flex-col gap-2">
         <label htmlFor={name}>{label}</label>
-        <div className={`w-fit rounded-md p-px relative overflow-hidden z-1000 after:rounded-full
+        <div className={`w-fit rounded-md p-[2px] relative overflow-hidden z-1000 after:rounded-full
           after:bg-conic-0 ${hasAnimation ? `after:animate-spin-slow after:from-purple-500 after:to-violet-400` : `after:from-purple-200 after:to-violet-100`}
           after:absolute after:w-50 after:h-50 after:left-[50%] after:top-[50%] after:-translate-[50%] after:z-10`}>
+          <Chevron className={`arrow absolute rotate-90 size-4 right-[.125rem] top-[50%] -translate-[50%] z-1000 transition-all transition-ease-in-out ${isMouseOver ? 'fill-indigo-800' : 'fill-indigo-100'}`} />
           <select 
             defaultValue={selected} 
             onChange={e => onSetSelected(e.target.value)} 
@@ -38,8 +42,12 @@ export default function Dropdown({
             id={name} 
             aria-label={label}
             className="dropdown relative z-100 outline-none cursor-pointer appearance-none
-              p-2 bg-indigo-700 text-fuchsia-50 rounded-md
-              dark:bg-indigo-800 dark:text-indigo-100">
+              p-2 pr-8 bg-indigo-700 text-indigo-50 rounded-md
+              hover:bg-indigo-100 hover:text-indigo-800 transition-all transition-ease-in-out
+              dark:bg-indigo-800 dark:text-indigo-100"
+            onMouseEnter={() => setMouseOver(true)}
+            onMouseLeave={() => setMouseOver(false)}
+              >
             <option value="" disabled>{placeholder} </option>  
             {options.map(option => 
               <option value={option.value} key={option.value}>{option.label}</option>
--- components/input.tsx
+++ components/input.tsx
@@ -21,7 +21,7 @@ export default function Input({
     return (
         <div className="flex flex-col gap-2">
             <label htmlFor={name}>{label}</label>
-            <div className={`p-px w-fit overflow-hidden relative rounded-md
+            <div className={`p-[2px] w-fit overflow-hidden relative rounded-md
                 after:bg-conic-0 ${hasAnimation ? `after:animate-spin-slow after:from-purple-500 after:to-violet-400` : `after:from-purple-200 after:to-violet-100`}
                 after:absolute after:w-60 after:h-60 after:left-[50%] after:top-[50%] after:-translate-[50%] after:z-10`}>
                 <input className="relative bg-black p-2 rounded-md outline-none z-1000" 
